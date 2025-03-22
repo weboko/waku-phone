4e8552a874e896aec76e1cf38bb684266587edf4
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2025-03-22 22:22:24 +0530

    dialing explicitly to peers seem to work better

--- src/lib/waku.ts
+++ src/lib/waku.ts
@@ -92,6 +92,11 @@ export class Waku {
     
     const node = await createLightNode({
       defaultBootstrap: false,
+/*       bootstrapPeers:[
+        "/dns4/waku-test.bloxy.one/tcp/8095/wss/p2p/16Uiu2HAmSZbDB7CusdRhgkD81VssRjQV5ZH13FbzCGcdnbbh6VwZ",
+        "/dns4/vps-aaa00d52.vps.ovh.ca/tcp/8000/wss/p2p/16Uiu2HAm9PftGgHZwWE3wzdMde4m3kT2eYJFXLZfGoSED3gysofk"
+      ], */
+      //numPeersToUse: 1,
       networkConfig: {
         clusterId: 42,
         shards: [0]
